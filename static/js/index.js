11f2dda71a2d7b1e5a22063fd775d2cf4a3ac460
daryateryukhova <teryukhovadaria@gmail.com>
2021-05-11 21:39:16 +0300

    lab4 100%

---
+++ static/js/index.js
@@ -0,0 +1,38 @@
+function showInfoMessage() {
+  let page = document.querySelector("[data-is-set-user]");
+  if (page.dataset.state) {
+    Swal.fire(
+      "Я это сделал, а " +
+        names +
+        " тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”"
+    );
+  } else {
+    Swal.fire({
+      title: "Введите имя",
+      input: "text",
+      inputAttributes: {
+        autocapitalize: "off",
+      },
+      showCancelButton: true,
+      cancelButtonText: "Отмена",
+      confirmButtonText: "Ввести",
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          "Я это сделал, а " +
+            result.value +
+            " тебе от меня совет: “ Поступай правильно — поступай в ВятГУ!”"
+        );
+        names = result.value;
+        page.dataset.isSetUser = true;
+      }
+    });
+  }
+}
+
+document.querySelectorAll("[data-info-message-button]").forEach((button) => {
+  button.addEventListener("click", () => {
+    showInfoMessage();
+  });
+});
